8cddf6cdc514de9a9e439bf592165059b11bb00b
Christian Beam <cbeam@frontlineinsurance.com>
2022-09-28 13:07:54 -0400

    predeploy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename='/gtm-project'>
             <App />
         </BrowserRouter>
     </React.StrictMode>
